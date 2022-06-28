c9ce79ea515da45a247682b90526c3e5ebc6c5f8
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-06-28 10:31:43 -0400

    commit tree cd222c7b030b6ccdc9f29aec58516559b653e964

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = (num1 * num2);
+
+var random = (Math.floor(Math.random() * 10 ) + 1);
+console.log(random);
+
+
+var num3 = 20;
+var num4 = 5;
+var mod = (num3 / num4);
+
+var max = Math.max(1 , 5, 10 , 15, 20);
